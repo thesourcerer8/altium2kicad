289921219cb02d7fb42fe389a9b3c0594dc1d927
thesourcerer8 <pg@futureware.at>
2016-06-20 20:10:12 +0200

    Fixed error messages for missing RADIUS_FRAC

--- convertschema.pl
+++ convertschema.pl
@@ -581,7 +581,7 @@ EOF
         my $x=($d{'LOCATION.X'}*$f)-$relx;
 		my $y=($d{'LOCATION.Y'}*$f)-$rely;
 		($x,$y)=rotate($x,$y,$partorientation{$globalp});
-		my $r=int(($d{'RADIUS'}||0)+($d{'RADIUS_FRAC'}/100000.0))*$f;
+		my $r=int(($d{'RADIUS'}||0)+(($d{'RADIUS_FRAC'}||0)/100000.0))*$f;
 		my $sa="0"; $sa="$1$2" if(defined($d{'STARTANGLE'}) && $d{'STARTANGLE'}=~m/(\d+)\.(\d)(\d+)/);
 		my $ea="0"; $ea="$1$2" if(defined($d{'ENDANGLE'}) && $d{'ENDANGLE'}=~m/(\d+)\.(\d)(\d+)/);
 		my @liste=();
